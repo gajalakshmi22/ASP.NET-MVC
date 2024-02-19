e69331c0ac8bf43a9e0c22c0449426516624ea73
gajalakshmi22 <gaja.sightspectrum@gmail.com>
2024-02-19 15:27:31 +0530

    first commit

---
+++ MVCFileDemo/Common/TrackExecutionTime.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace MVCFileDemo.Common
+{
+    public class TrackExecutionTime : ActionFilterAttribute, IExceptionFilter
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string message = "\n" + filterContext.ActionDescriptor.ControllerDescriptor.ControllerName +
+                "->" + filterContext.ActionDescriptor.ActionName + "-> OnActionExecuting \t- " +
+                DateTime.Now.ToString() + "\n";
+            LogExecutionTime(message);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            string message = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName +
+                "->" + filterContext.ActionDescriptor.ActionName + "-> OnActionExecuted \t- " +
+                DateTime.Now.ToString() + "\n";
+            LogExecutionTime(message);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            string message = filterContext.RouteData.Values["controller"].ToString() +
+               "->" + filterContext.RouteData.Values["action"].ToString() + "-> OnResultExecuting \t- " +
+               DateTime.Now.ToString() + "\n";
+            LogExecutionTime(message);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            string message = filterContext.RouteData.Values["controller"].ToString() +
+               "->" + filterContext.RouteData.Values["action"].ToString() + "->OnResultExecuted \t- " +
+               DateTime.Now.ToString() + "\n";
+            LogExecutionTime(message);
+            LogExecutionTime("---------------------------------------------------------------");
+        }
+
+        public void OnException(ExceptionContext filterContext)
+        {
+            string message = filterContext.RouteData.Values["controller"].ToString() +
+               "->" + filterContext.RouteData.Values["action"].ToString() + "->" + filterContext.Exception.Message + " \t- " +
+               DateTime.Now.ToString() + "\n";
+            LogExecutionTime(message);
+            LogExecutionTime("---------------------------------------------------------------");
+        }
+        private void LogExecutionTime(string data)
+        {
+            File.AppendAllText(HttpContext.Current.Server.MapPath("~/Data/Data.txt"), data);
+        }
+    }
+}
---
+++ MVCFileDemo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCFileDemo.Common;
+
+namespace MVCFileDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [TrackExecutionTime]
+        public string Index()
+        {
+            return "Index action invoked";
+        }
+
+        [TrackExecutionTime]
+        public string Welcome()
+        {
+            throw new Exception("Exception Occured");
+        }
+    }
+}
